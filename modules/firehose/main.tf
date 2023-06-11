f2ffc309d695d42857a93553feb58783f0e5757a
guyrenny <guy.renny@coralogix.com>
2023-06-11 11:33:54 +0300

    check this commit before the changes

--- modules/firehose/main.tf
+++ modules/firehose/main.tf
@@ -13,7 +13,7 @@ locals {
       url = "https://firehose-ingress.coralogix.us/firehose" 
     } 
     "singapore" = {
-      url = "https://firehose-ingress.coralogixsg.com/firehose" 
+      url = "https://firehose-ingress.coralogixsg.com/firehose"  
     }  
     "ireland" = { 
       url = "https://firehose-ingress.coralogix.com/firehose"  
--- modules/s3/main.tf
+++ modules/s3/main.tf
@@ -15,7 +15,7 @@ locals {
     License  = "Apache-2.0"     
   }    
 }   
-   
+    
 data "aws_region" "this" {} 
 
 data "aws_s3_bucket" "this" {
