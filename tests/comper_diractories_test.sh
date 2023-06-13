f6f2e0a6d06670c8ba8bf54444526d977f07dd09
guyrenny <127672145+guyrenny@users.noreply.github.com>
2023-06-13 15:37:45 +0300

    Update comper_diractories_test.sh

--- tests/comper_diractories_test.sh
+++ tests/comper_diractories_test.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-dir1="../tests"
-dir2="../modules"
+dir1="tests"
+dir2="modules"
 
 dir1_dirs=$(find "$dir1" -type d -maxdepth 1 -mindepth 1 -exec basename {} \;)
 dir2_dirs=$(find "$dir2" -type d -maxdepth 1 -mindepth 1 -exec basename {} \;)
