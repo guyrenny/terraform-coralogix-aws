c215ad42ae3cbe3c05214fc3f7111e3fe81278fc
guyrenny <127672145+guyrenny@users.noreply.github.com>
2023-06-13 15:15:43 +0300

    Create comper_diractories_test.sh

---
+++ tests/comper_diractories_test.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+dir1="../tests"
+dir2="../modules"
+
+dir1_dirs=$(find "$dir1" -type d -maxdepth 1 -mindepth 1 -exec basename {} \;)
+dir2_dirs=$(find "$dir2" -type d -maxdepth 1 -mindepth 1 -exec basename {} \;)
+
+output=$(echo "${dir2_dirs[@]} ${dir1_dirs[@]}" | tr ' ' '\n' | sort | uniq -u)
+
+echo $output
