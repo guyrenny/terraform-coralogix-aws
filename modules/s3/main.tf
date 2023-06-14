25cef35b12879695f653295a11717370e66a7a57
guyrenny <127672145+guyrenny@users.noreply.github.com>
2023-06-14 16:43:13 +0300

    Update main.tf

--- modules/s3/main.tf
+++ modules/s3/main.tf
@@ -8,7 +8,7 @@ module "locals" {
 locals { 
   sns_enable = var.integration_type == "s3-sns" || var.integration_type == "cloudtrail-sns" ? true : false
 }
-
+ 
 data "aws_region" "this" {}
 
 data "aws_caller_identity" "this" {}
