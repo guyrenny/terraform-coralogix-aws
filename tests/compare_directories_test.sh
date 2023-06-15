1d203e683fbe02ebd6608a829d12cf61c1517e0d
guyrenny <guy.renny@coralogix.com>
2023-06-15 12:10:03 +0300

    checnge the code

--- tests/compare_directories_test.sh
+++ tests/compare_directories_test.sh
@@ -10,4 +10,18 @@ dir2_dirs=$(find "$dir2" -type d -maxdepth 1 -mindepth 1 -exec basename {} \;)
 
 output=$(echo "${dir2_dirs[@]} ${dir1_dirs[@]}" | tr ' ' '\n' | sort | uniq -u)
 
-echo $output
+missing_directoies=""
+
+output_array=()
+while IFS= read -r line; do
+  output_array+=("$line")
+done < <(echo "${dir2_dirs[@]}" "${dir1_dirs[@]}" | tr ' ' '\n' | sort | uniq -u)
+
+# Print the elements of the output array
+for dir in "${output_array[@]}"; do
+  if ! echo $dir1_dirs | grep -q "$dir" ; then
+    missing_directoies+="$dir  "
+  fi
+done
+
+echo $missing_directoies
