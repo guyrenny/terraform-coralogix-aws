0f294054f8fd1ed9e81adef25dd610293a6fb600
guyrenny <guy.renny@coralogix.com>
2023-06-15 14:15:09 +0300

    made the changes to the dir script

--- tests/compare_directories_test.sh
+++ tests/compare_directories_test.sh
@@ -4,24 +4,42 @@
 
 dir1="tests"
 dir2="modules"
+missing_tests_directoies=""
+missing_tests_files=""
 
 dir1_dirs=$(find "$dir1" -type d -maxdepth 1 -mindepth 1 -exec basename {} \;)
 dir2_dirs=$(find "$dir2" -type d -maxdepth 1 -mindepth 1 -exec basename {} \;)
-
-output=$(echo "${dir2_dirs[@]} ${dir1_dirs[@]}" | tr ' ' '\n' | sort | uniq -u)
-
-missing_directoies=""
 
 output_array=()
 while IFS= read -r line; do
   output_array+=("$line")
 done < <(echo "${dir2_dirs[@]}" "${dir1_dirs[@]}" | tr ' ' '\n' | sort | uniq -u)
 
+test_directories=()
+while IFS= read -r line; do
+  test_directories+=("$line")
+done < <(echo "${dir1_dirs[@]}" | tr ' ' '\n')
+
+for dir in "${test_directories[@]}"; do
+    if [[ $dir2_dirs =~ "$dir" ]]  && ! [[ -n $(ls "tests/$dir") ]]; then
+        missing_tests_files+="$dir "
+    fi
+done
+
 # Print the elements of the output array
 for dir in "${output_array[@]}"; do
   if ! [[ $dir1_dirs =~ "$dir" ]] && [[ "$dir" != "locals_variables" ]]; then
-    missing_directoies+="$dir  "
+    missing_tests_directoies+="$dir  "
   fi
 done
 
-echo $missing_directoies
+if [[ -n $missing_tests_directoies ]] && [[ -n $missing_tests_files ]]; then
+    echo "[ERROR] Tests that are missing for modules: $missing_tests_directoies. Tests files that are missing: $missing_tests_files"
+elif [[ -n $missing_tests_directoies ]]; then 
+    echo "[ERROR] Tests that are missing for modules: $missing_tests_directoies"
+elif [[ -n $missing_tests_files ]]; then  
+    echo "[ERROR] Tests that are missing template file: $missing_tests_files"
+elif [[ -z $missing_tests_directoies ]] && [[ -z $missing_tests_files ]]; then
+    echo ""
+fi
+
