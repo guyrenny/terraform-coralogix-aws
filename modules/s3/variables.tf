d203abb48d111054ee1f8e5836282abd9f483951
guyrenny <127672145+guyrenny@users.noreply.github.com>
2023-06-19 11:33:09 +0300

    Update variables.tf

--- modules/s3/variables.tf
+++ modules/s3/variables.tf
@@ -18,6 +18,7 @@ variable "private_key" {
   description = "The Coralogix private key which is used to validate your authenticity"
   type        = string
   sensitive   = true
+  default     = ""
 }
 
 variable "ssm_enable" {
