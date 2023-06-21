2c654c0e29eded3c4210c7928d4b704dcac6dfd0
guyrenny <guy.renny@coralogix.com>
2023-06-21 13:45:42 +0300

    test

--- tests/s3/s3.tf
+++ tests/s3/s3.tf
@@ -17,10 +17,15 @@ module "s3" {
 
   coralogix_region   = "Europe"
   ssm_enable         = "false"
- # private_key        = "${{ secrets.TESTING_PRIVATE_KEY }}"
+  private_key        = var.private_key
   layer_arn          = "arn:aws:lambda:us-east-1:035955823196:layer:coralogix-ssmlayer:1"
   application_name   = "s3"
   subsystem_name     = "logs"
   s3_bucket_name     = "github-action-bucket-testing"
   integration_type   = "s3"
+}
+
+variable "private_key" {
+  type    = string
+  default = "from-test"
 }
