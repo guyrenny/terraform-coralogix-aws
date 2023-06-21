9945b07f6d7b56b54a8e5c96683f88c3f17f2737
guyrenny <127672145+guyrenny@users.noreply.github.com>
2023-06-21 11:33:25 +0300

    Update s3.tf

--- tests/s3/s3.tf
+++ tests/s3/s3.tf
@@ -17,15 +17,10 @@ module "s3" {
 
   coralogix_region   = "Europe"
   ssm_enable         = "false"
-#   private_key         = "test"
+  private_key        = "test"
   layer_arn          = "arn:aws:lambda:us-east-1:035955823196:layer:coralogix-ssmlayer:1"
   application_name   = "s3"
   subsystem_name     = "logs"
   s3_bucket_name     = "github-action-bucket-testing"
   integration_type   = "s3"
-}
-variable "private_key" {
-  description = "Private key for authentication"
-  type        = string
-  default     = "12"
 }
